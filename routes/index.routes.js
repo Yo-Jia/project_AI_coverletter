2ecebf67e3e2ceb80a125240f654ca44ae103732
Rohan Singh <rsingh@faruse.com>
2023-05-20 09:10:16 +0200

    increase the token and temperature

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -206,10 +206,10 @@ router.post("/profile/:username/create", isLoggedIn, async (req, res) => {
       try {
         const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
           prompt: prompt,
-          max_tokens: 100,
+          max_tokens: 1000,
           n: 1,
           stop: null,
-          temperature: 0.4,
+          temperature: 0.7,
         }, {
           headers: {
             'Content-Type': 'application/json',
