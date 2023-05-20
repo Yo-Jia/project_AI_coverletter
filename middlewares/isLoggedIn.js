41a6ef852a654e7c97dc2206e0f300bb02e88a86
Yo-Jia Chen <donna880815@gmail.com>
2023-05-20 10:46:13 +0200

    delete everything

--- middlewares/isLoggedIn.js
+++
@@ -1,9 +0,0 @@
-const isLoggedIn = (req,res,next) =>{
-    if(req.session.user){
-        next();
-        return;
-    }
-    res.redirect("/auth/login")
-}
-
-module.exports = isLoggedIn
--- middlewares/isLoggedOut.js
+++
@@ -1,9 +0,0 @@
-const isLoggedOut = (req,res,next) =>{
-    if(!req.session.user){
-        next();
-        return;
-    }
-    res.redirect(`/profile/${req.session.user.username}`)
-}
-
-module.exports = isLoggedOut;
